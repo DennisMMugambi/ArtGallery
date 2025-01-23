e40a3fde6e5a28681fe5915abdd1b5a7a9c79267
DennisMMugambi <dennis.mutwiri.mugambi@gmail.com>
2025-01-23 16:18:49 +0300

    converts landing page design

--- androidApp/src/main/java/com/example/artgallery/android/theme/Typography.kt
+++ androidApp/src/main/java/com/example/artgallery/android/theme/Typography.kt
@@ -3,27 +3,25 @@ package com.example.artgallery.android.theme
 import androidx.compose.material3.Typography
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import com.example.artgallery.android.R
 
 val typography = Typography(
     displayLarge = TextStyle(
-        fontFamily = FontFamily(Font(R.font.comicneue_bold)),
-        fontSize = 24.sp,
+        fontFamily = FontFamily.SansSerif,
+        fontSize = 20.sp,
         fontWeight = FontWeight.Bold,
         color = Color.White
     ),
     displayMedium = TextStyle(
-        fontFamily = FontFamily(Font(R.font.comicneue_regular)),
+        fontFamily = FontFamily.SansSerif,
         fontSize = 20.sp,
         color = Color.White
     ),
     displaySmall = TextStyle(
-        fontFamily = FontFamily(Font(R.font.comicneue_light)),
-        fontSize = 12.sp,
+        fontFamily = FontFamily.SansSerif,
+        fontSize = 16.sp,
         color = Color.White
     )
 )
