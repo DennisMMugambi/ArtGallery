7a120ea55c6997230198e5014e46fbb41caacd3f
DennisMMugambi <dennis.mutwiri.mugambi@gmail.com>
2025-01-24 15:09:39 +0300

    completes login screen

--- androidApp/src/main/java/com/example/artgallery/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/artgallery/android/MainActivity.kt
@@ -3,15 +3,17 @@ package com.example.artgallery.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.artgallery.android.ui.login.LandingScreen
-import com.example.artgallery.android.ui.main.MainView
+import com.example.artgallery.android.navigation.AppNavigation
+import com.example.artgallery.android.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-           LandingScreen()
+            AppTheme {
+                AppNavigation()
+            }
         }
     }
 }
---
+++ androidApp/src/main/java/com/example/artgallery/android/ui/login/Authentication.kt
@@ -0,0 +1,8 @@
+package com.example.artgallery.android.ui.login
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Authentication() {
+
+}
