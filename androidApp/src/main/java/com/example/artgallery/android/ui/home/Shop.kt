ead82d5a7003b77932a464c680f9ac163db844f0
DennisMMugambi <dennis.mutwiri.mugambi@gmail.com>
2025-02-25 10:49:52 +0300

    adds coming soon text for shop screen

--- androidApp/src/main/java/com/example/artgallery/android/ui/home/Shop.kt
+++ androidApp/src/main/java/com/example/artgallery/android/ui/home/Shop.kt
@@ -1,8 +1,25 @@
 package com.example.artgallery.android.ui.home
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.artgallery.android.R
 
 @Composable
 fun ShopScreen() {
 
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(text = stringResource(id = R.string.coming_soon), fontWeight = FontWeight.Bold, fontSize = 20.sp)
+    }
 }
