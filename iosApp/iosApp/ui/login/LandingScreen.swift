dd7a8b1e4b667cb86925823e2789f0182d241c1a
DennisMMugambi <dennis.mutwiri.mugambi@gmail.com>
2025-03-03 11:06:12 +0300

    Converts iOS landing screen

---
+++ iosApp/iosApp/ui/login/LandingScreen.swift
@@ -0,0 +1,19 @@
+//
+//  LandingScreen.swift
+//  iosApp
+//
+//  Created by Dennis Mugambi on 26/02/2025.
+//  Copyright © 2025 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandingScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandingScreen()
+}
