d112097dd0784efa648aef7a59780b3959c31f90
DennisMMugambi <dennis.mutwiri.mugambi@gmail.com>
2025-03-04 12:06:02 +0300

    converts get started screen for iOS

---
+++ iosApp/iosApp/ui/components/ExistingAccountComponent.swift
@@ -0,0 +1,19 @@
+//
+//  ExistingAccountComponent.swift
+//  iosApp
+//
+//  Created by Dennis Mugambi on 04/03/2025.
+//  Copyright © 2025 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct ExistingAccountComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExistingAccountComponent()
+}
