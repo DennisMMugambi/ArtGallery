a11f721eafa4ec1c8f70ba5806e56c631ced8d25
DennisMMugambi <dennis.mutwiri.mugambi@gmail.com>
2025-03-10 11:12:47 +0300

    creates home destinations screens

---
+++ iosApp/iosApp/ui/home/TabScreen.swift
@@ -0,0 +1,19 @@
+//
+//  TabScreen.swift
+//  iosApp
+//
+//  Created by Dennis Mugambi on 10/03/2025.
+//  Copyright © 2025 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct TabScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabScreen()
+}
---
+++ iosApp/iosApp/ui/home/profile/ProfileScreen.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileScreen.swift
+//  iosApp
+//
+//  Created by Dennis Mugambi on 10/03/2025.
+//  Copyright © 2025 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileScreen()
+}
