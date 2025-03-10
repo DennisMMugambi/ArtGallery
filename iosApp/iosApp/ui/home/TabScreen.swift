1c4447ecd5216f9553ea51d01b5b08bd87cffbc0
DennisMMugambi <dennis.mutwiri.mugambi@gmail.com>
2025-03-10 16:13:28 +0300

    adds conditional searchbar to home screen

--- iosApp/iosApp/ui/home/TabScreen.swift
+++ iosApp/iosApp/ui/home/TabScreen.swift
@@ -11,16 +11,50 @@ import SwiftUI
 struct TabScreen: View {
     
     @State private var tab_selection = 0
+    @State private var searchText = ""
+    @State private var isSearchActive = false
     
     var body: some View {
-        
-        VStack {
-            
+           
+        if (tab_selection == 0) {
+            VStack {
+                TabView(selection: $tab_selection) {
+                    
+                    HomeScreen()
+                        .tabItem {
+                            Image(systemName: "house")
+                        }.tag(0)
+                    
+                    FavoriteScreen().tabItem {
+                        Image(systemName: "star.circle")
+                    }.tag(1)
+                    
+                    ShopScreen().tabItem {
+                        Image(systemName: "tray")
+                    }.tag(2)
+                    
+                    ProfileScreen().tabItem {
+                        Image(systemName: "person.circle")
+                    }.tag(3)
+                }
+                
+            }
+            .navigationBarBackButtonHidden(true)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                        Text("Welcome to Art Gallery")
+                        .font(.custom(Constants.Fonts.poppinsMedium, size: 22))
+                        .bold()
+                    }
+            }
+            .searchable(text: $searchText, isPresented: $isSearchActive, prompt: "Search exhibitions, artworks")
+        } else {
             TabView(selection: $tab_selection) {
                 
-                HomeScreen().tabItem {
-                    Image(systemName: "house")
-                }.tag(0)
+                HomeScreen()
+                    .tabItem {
+                        Image(systemName: "house")
+                    }.tag(0)
                 
                 FavoriteScreen().tabItem {
                     Image(systemName: "star.circle")
@@ -34,8 +68,8 @@ struct TabScreen: View {
                     Image(systemName: "person.circle")
                 }.tag(3)
             }
+            .navigationBarBackButtonHidden(true)
         }
-        //.navigationBarBackButtonHidden(true)
     }
 }
 
