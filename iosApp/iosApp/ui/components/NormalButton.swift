40006c7e94fc49ef7af96b54b9b3438d469a331d
DennisMMugambi <dennis.mutwiri.mugambi@gmail.com>
2025-03-18 14:54:33 +0300

    creates exhibition items scrollable section for iOS

---
+++ iosApp/iosApp/ui/components/NormalButton.swift
@@ -0,0 +1,19 @@
+//
+//  NormalButton.swift
+//  iosApp
+//
+//  Created by Dennis Mugambi on 18/03/2025.
+//  Copyright © 2025 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct NormalButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NormalButton()
+}
