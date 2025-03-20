3807b613146cfa5fda198292a89d6d1a831ef6ce
DennisMMugambi <dennis.mutwiri.mugambi@gmail.com>
2025-03-20 12:49:16 +0300

    completes home screen design for iOS

---
+++ iosApp/iosApp/ui/components/SectionDescription.swift
@@ -0,0 +1,19 @@
+//
+//  SectionDescription.swift
+//  iosApp
+//
+//  Created by Dennis Mugambi on 20/03/2025.
+//  Copyright © 2025 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct SectionDescription: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SectionDescription()
+}
