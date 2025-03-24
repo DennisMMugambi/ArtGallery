a2fc148dde7700642f6891591a5f402c0b8a906e
DennisMMugambi <dennis.mutwiri.mugambi@gmail.com>
2025-03-24 14:44:03 +0300

    converts profile screen

---
+++ iosApp/iosApp/ui/components/ProfileItem.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileItem.swift
+//  iosApp
+//
+//  Created by Dennis Mugambi on 24/03/2025.
+//  Copyright © 2025 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileItem()
+}
