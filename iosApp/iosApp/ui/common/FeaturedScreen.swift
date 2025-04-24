d7a6aa171f4518cf677aee7520b46d931f49af86
DennisMMugambi <dennis.mutwiri.mugambi@gmail.com>
2025-04-24 10:51:48 +0300

    creates Featured Destination Screen

---
+++ iosApp/iosApp/ui/common/FeaturedScreen.swift
@@ -0,0 +1,19 @@
+//
+//  FeaturedScreen.swift
+//  iosApp
+//
+//  Created by Dennis Mugambi on 24/04/2025.
+//  Copyright © 2025 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct FeaturedScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeaturedScreen()
+}
