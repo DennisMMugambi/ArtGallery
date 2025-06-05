f54529d975826cbe749f01f3bd84dfd86e343ca2
DennisMMugambi <dennis.mutwiri.mugambi@gmail.com>
2025-06-05 09:28:49 +0300

    updates kotlin and gradle agp versions for compatibility with xcode 16.3

---
+++ shared/build/cocoapods/synthetic/ios/build/Pods.build/Debug-iphonesimulator/SDWebImage.build/Objects-normal/arm64/SDWebImage_vers.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/dennismugambi/StudioProjects/ArtGallery/shared/build/cocoapods/synthetic/ios/build/Pods.build/Debug-iphonesimulator/SDWebImage.build/module.modulemap \
+  /Users/dennismugambi/StudioProjects/ArtGallery/shared/build/cocoapods/synthetic/ios/build/Pods.build/Debug-iphonesimulator/SDWebImage.build/DerivedSources/SDWebImage_vers.c
