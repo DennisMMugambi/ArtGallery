9d5d635fa7ea4676983856fe003ac67174a56fff
DennisMMugambi <dennis.mutwiri.mugambi@gmail.com>
2025-06-05 10:28:18 +0300

    hides google server client id for google sign in - in gradle properties

--- shared/build/cocoapods/synthetic/ios/build/Pods.build/Debug-iphoneos/SDWebImage.build/Objects-normal/arm64/SDFileAttributeHelper.d
+++
@@ -1,8 +0,0 @@
-dependencies: \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/usr/include/Darwin.modulemap \
-  /Users/dennismugambi/StudioProjects/ArtGallery/shared/build/cocoapods/synthetic/ios/build/Pods.build/Debug-iphoneos/SDWebImage.build/module.modulemap \
-  /Users/dennismugambi/StudioProjects/ArtGallery/shared/build/cocoapods/synthetic/ios/Pods/SDWebImage/SDWebImage/Private/SDFileAttributeHelper.m \
-  /Users/dennismugambi/StudioProjects/ArtGallery/shared/build/cocoapods/synthetic/ios/Pods/Target\ Support\ Files/SDWebImage/SDWebImage-prefix.pch \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/System/Library/Frameworks/UIKit.framework/Modules/module.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/System/Library/Frameworks/Foundation.framework/Modules/module.modulemap \
-  /Users/dennismugambi/StudioProjects/ArtGallery/shared/build/cocoapods/synthetic/ios/Pods/SDWebImage/SDWebImage/Private/SDFileAttributeHelper.h
