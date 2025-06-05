cfe8b5928a16baa4ea859f01a4f8b355be5dfa4c
DennisMMugambi <dennis.mutwiri.mugambi@gmail.com>
2025-06-05 10:28:18 +0300

    hides google server client id for google sign in - in gradle properties

--- shared/build/cocoapods/synthetic/ios/build/Pods.build/Debug-iphoneos/SDWebImage.build/Objects-normal/arm64/SDWebImage_vers.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/dennismugambi/StudioProjects/ArtGallery/shared/build/cocoapods/synthetic/ios/build/Pods.build/Debug-iphoneos/SDWebImage.build/module.modulemap \
-  /Users/dennismugambi/StudioProjects/ArtGallery/shared/build/cocoapods/synthetic/ios/build/Pods.build/Debug-iphoneos/SDWebImage.build/DerivedSources/SDWebImage_vers.c
--- shared/build/cocoapods/synthetic/ios/build/Pods.build/Debug-iphonesimulator/SDWebImage.build/Objects-normal/x86_64/NSBezierPath+SDRoundedCorners.d
+++
@@ -1,8 +0,0 @@
-dependencies: \
-  /Users/dennismugambi/StudioProjects/ArtGallery/shared/build/cocoapods/synthetic/ios/build/Pods.build/Debug-iphonesimulator/SDWebImage.build/module.modulemap \
-  /Users/dennismugambi/StudioProjects/ArtGallery/shared/build/cocoapods/synthetic/ios/Pods/SDWebImage/SDWebImage/Private/NSBezierPath+SDRoundedCorners.m \
-  /Users/dennismugambi/StudioProjects/ArtGallery/shared/build/cocoapods/synthetic/ios/Pods/Target\ Support\ Files/SDWebImage/SDWebImage-prefix.pch \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/System/Library/Frameworks/UIKit.framework/Modules/module.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/usr/include/TargetConditionals.modulemap \
-  /Users/dennismugambi/StudioProjects/ArtGallery/shared/build/cocoapods/synthetic/ios/Pods/SDWebImage/SDWebImage/Private/NSBezierPath+SDRoundedCorners.h \
-  /Users/dennismugambi/StudioProjects/ArtGallery/shared/build/cocoapods/synthetic/ios/Pods/SDWebImage/SDWebImage/Core/SDWebImageCompat.h
